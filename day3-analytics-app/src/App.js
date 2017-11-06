a96a501dc35447b6c331e8a526f6835dc84b2808
Jan Henrik Overland <jan@dhis2.org>
2017-11-06 11:19:22 +0300

    plugins

--- day3-analytics-app/src/App.js
+++ day3-analytics-app/src/App.js
@@ -1,84 +1,74 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { getData } from './actions';
+import React, { Component } from 'react';
 import './App.css';
 
-let selectedValue = null;
+const reportTablePlugin = global.reportTablePlugin;
+const chartPlugin = global.chartPlugin;
 
-const Selector = ({ onClick }) => {
-    const clickHandler = (e) => {
-        if (e.target.value !== selectedValue) {
-            onClick(e.target.value);
-            selectedValue = e.target.value;
-        }
-    };
+const url = 'http://localhost:8080';
+const username = 'admin';
+const password = 'district';
 
-    return (
-        <section>
-            <select onClick={clickHandler} defaultValue="">
-                <option hidden value="">Select period</option>
-                <option value="2017">Fixed: 2017</option>
-                <option value="201701">Fixed: January 2017</option>
-                <option value="201701;201702;201703">Fixed: January, February, March 2017</option>
-                <option value="2017Q3">Fixed: 3rd quarter 2017</option>
-                <option value="LAST_12_MONTHS">Relative: Last 12 months</option>
-                <option value="LAST_4_QUARTERS">Relative: Last 4 quarters</option>
-                <option value="LAST_YEAR">Relative: Last year</option>
-            </select>
-        </section>
-    );
-};
+class App extends Component {
+    componentDidMount() {
+        reportTablePlugin.url = url;
+        reportTablePlugin.username = username;
+        reportTablePlugin.password = password;
 
-const ValueTable = ({ data, isLoading }) => {
-    if (!data) {
-        return 'No values here';
+        chartPlugin.url = url;
+        chartPlugin.username = username;
+        chartPlugin.password = password;
+
+        reportTablePlugin.load({
+            id: 'C0rhAq1oklh',
+            el: 'table1'
+        });
+
+        reportTablePlugin.load({
+            columns: [
+                {
+                    dimension: 'dx',
+                    items: [
+                        {
+                            id: 'Uvn6LCg7dVU'
+                        }
+                    ]
+                }
+            ],
+            rows: [
+                {
+                    dimension: 'pe',
+                    items: [
+                        {
+                            id: 'LAST_12_MONTHS'
+                        }
+                    ]
+                }
+            ],
+            el: 'table2'
+        });
+
+        chartPlugin.load({
+            id: 'DkPKc1EUmC2',
+            el: 'chart1'
+        });
     }
-
-    if (isLoading) {
-        return 'Loading...';
+    render() {
+        return (
+            <div>
+                <h1>My web portal</h1>
+                <section>
+                    <h2>Pivot table by favorite id</h2>
+                    <div id="table1"></div>
+                    <h2>Pivot table by config</h2>
+                    <div id="table2"></div>
+                </section>
+                <section>
+                    <h2>Chart by favorite id</h2>
+                    <div id="chart1"></div>
+                </section>
+            </div>
+        );
     }
+}
 
-    const { headers, metaData, rows } = data;
-
-    return (
-        <section>
-            <table>
-                <thead>
-                <tr>
-                    {headers.map((header, index) => <th key={index}>{header.column}</th>)}
-                </tr>
-                </thead>
-                <tbody>
-                {rows.map((row, index) =>
-                    <tr key={index}>{row.map((value, index) => <td key={index}>{(metaData.items[value] || {}).name || value}</td>)}</tr>
-                )}
-                </tbody>
-            </table>
-        </section>
-    );
-};
-
-const App = ({ data, isLoading, onClick }) => (
-    <div>
-        <Selector onClick={ onClick } />
-        <ValueTable data={ data } isLoading={ isLoading } />
-    </div>
-);
-
-App.contextTypes = {
-    store: PropTypes.object,
-};
-
-const mapStateToProps = state => ({
-    isLoading: state.isLoading,
-    data: state.data
-});
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    onClick: value => value && dispatch(getData(value))
-});
-
-const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
-
-export default AppContainer;
+export default App;
--- day3-analytics-app/src/index.js
+++ day3-analytics-app/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './configureStore';
-import AppContainer from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Provider store={configureStore()}>
-        <AppContainer />
-    </Provider>,
+    <App />,
     document.getElementById('root')
 );
 
