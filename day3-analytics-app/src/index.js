6fa4e50acf619e0aca5ac2df62088fa6261aea7a
Edoardo Sabadelli <edoardo@dhis2.org>
2017-11-07 14:17:11 +0300

    Initialize d2 within the app

--- day3-analytics-app/src/index.js
+++ day3-analytics-app/src/index.js
@@ -5,11 +5,23 @@ import configureStore from './configureStore';
 import AppContainer from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <Provider store={configureStore()}>
-        <AppContainer />
-    </Provider>,
-    document.getElementById('root')
-);
+import { init } from 'd2/lib/d2';
+
+init({
+    baseUrl: 'http://localhost:8080/api',
+    schemas: [],
+    headers: { authentication: `Basic ${btoa('admin:district')}` }
+})
+.then((d2) => {
+    ReactDOM.render(
+        <Provider store={configureStore()}>
+            <AppContainer />
+        </Provider>,
+        document.getElementById('root')
+    );
+})
+.catch((error) => {
+    console.warn('Failed to initialize d2', error);
+});
 
 registerServiceWorker();
